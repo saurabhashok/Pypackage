69c3d2678fc454948f8e8d96f5d334a9cea3a757
saurabh ashok <saurabhashok93@gmail.com>
2021-05-06 15:00:32 +0000

    add package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='db _probability',
+      version='0.1',
+      description='Gaussian distributions and Binomial distribution',
+      packages=['db_probability'],
+      author ='Saurabh Ashoka',
+      author_email ='saurabhashok93@gmail.com',
+      zip_safe=False)
